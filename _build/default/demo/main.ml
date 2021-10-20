23416a524e2afb42da7f784461fcf96b77e1e6ed
Ved Kumar <vk262@cornell.edu>
2021-10-20 10:55:33 -0400

    almost demo

--- _build/default/demo/main.ml
+++ _build/default/demo/main.ml
@@ -1,27 +1,31 @@
 open Chess.Backend
 
-let write x = print_string x
+type demoset = (board_coord * board_coord * string) list
 
-let x = write "demo on moving knight from b1 to c3"
+let demo x y line =
+  print_endline line;
+  let res = demo x y in
+  match res with
+  | true -> print_endline "true\n"
+  | false -> print_endline "false\n"
 
-let res = demo { column = 1; row = 0 } { column = 2; row = 2 }
-
-let x = write (if res then "true\n" else "false\n")
+let rec democheck = function
+  | (start, finish, line) :: t ->
+      let _ = demo start finish line in
+      democheck t
+  | [] -> print_endline "complete.\n"
 
 let main () =
   ANSITerminal.print_string [ ANSITerminal.red ]
     "\n\nWelcome to the 3110 Text Adventure Game engine.\n";
-  print_endline "demo on moving knight from b1 to c3\n";
-  let res = demo { column = 1; row = 0 } { column = 2; row = 2 } in
-  match res with
-  | true -> print_endline "true\n"
-  | false -> (
-      print_endline "false\n";
-      print_endline "demo on moving knight from b1 to c3\n";
-      let res = demo { column = 1; row = 0 } { column = 2; row = 2 } in
-      match res with
-      | true -> print_endline "true\n"
-      | false -> print_endline "false\n")
+  let piecetests =
+    [
+      ( { column = 1; row = 0 },
+        { column = 2; row = 2 },
+        "demo on moving knight from b1 to c3\n" );
+    ]
+  in
+  democheck piecetests
 
 (* Execute the game engine. *)
 let () = main ()
