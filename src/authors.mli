b69911a8c2e3ae525b931bd46537ee6e2875191d
Neil Estrada <nae28@cornell.edu>
2021-10-20 18:39:29 -0400

    Install.txt and Authors compilation unit. Also fixed dune things

---
+++ src/authors.mli
@@ -0,0 +1,36 @@
+(************************************************************
+   Copyright (C) 2021 Cornell University.
+   Created by Michael Clarkson (mrc26@cornell.edu) and CS 3110 course staff.
+   You may not redistribute this assignment, distribute any derivatives,
+   or use it for commercial purposes.
+ ************************************************************)
+
+(* TODO: change the name and NetID below to your own, then delete this
+   TODO comment. *)
+
+(** CS 3110 Fall 2021 Assignment A2 Adventure
+
+    @author Neil Estrada (nae28) Andrew Cai (ayc57) Jason Hyun (jh986) Ved Kumar (vk262))*)
+
+(* TODO: complete the academic integrity statement below, then delete
+   this TODO comment. *)
+
+(************************************************************
+
+   Academic Integrity Statement
+
+   I, the person named in the author comment above, have fully reviewed the
+   course academic integrity policies.  I have adhered to those policies in
+   solving the assignment.
+
+   The policies do permit some limited collaboration among students currently
+   enrolled in the course. If I did engage in such collaboration, here is the
+   list of other students with whom I collaborated, and a brief summary of that
+   collaboration:
+
+   - Neil Estrada, Andrew Cai, Jason Hyun, Ved Kumar.
+
+ ************************************************************)
+
+val hours_worked : int
+(** [hours_worked] is the number of hours you worked on this assignment. *)
